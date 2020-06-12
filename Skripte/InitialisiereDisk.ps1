29a6554775a9a02fb39c1d1df58961656bca10c2
Sommerflug <57090418+Sommerflug@users.noreply.github.com>
2020-06-12 13:15:42 +0200

    Update InitialisiereDisk.ps1

--- Skripte/InitialisiereDisk.ps1
+++ Skripte/InitialisiereDisk.ps1
@@ -1,5 +1,11 @@
 Start-Transcript C:\transscript.txt
 
+# Set CD/DVD Drive to A:
+Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' |
+Select-Object -First 1 |
+Set-WmiInstance -Arguments @{DriveLetter='A:'}
+
+#Leere HD ermitteln -> initialisieren
 $disk = Get-Disk | Where-Object NumberOfPartitions -eq 0 | Select-Object -first 1
 
 $disk | 
